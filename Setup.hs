eeae5da53be5d35fca36066fea968df2ea75de02
Felix Paulusma <fpaulusma@zig.nl>
2020-08-24 16:57:03 +0200

    it works, just need to link the libnewrelic.a library when building your project (build it locally from 'newrelic/c-sdk')

--- Setup.hs
+++ Setup.hs
@@ -5,7 +5,6 @@ import Distribution.Simple.Setup (CleanFlags, ConfigFlags)
 import System.Directory (createDirectoryIfMissing, getCurrentDirectory, removeDirectoryRecursive, withCurrentDirectory)
 import System.Process (readProcess)
 
-import Debug.Trace (trace)
 
 main :: IO ()
 main =
@@ -32,34 +31,34 @@ newrelicSdkDir = "c-sdk-" <> newrelicVersion
 -- c-sdk so we have the include headers needed to compile the hsc file.
 makeLibNewrelic :: Args -> ConfigFlags -> IO HookedBuildInfo
 makeLibNewrelic _ _ = do
-  _ <- trace "create temp dir" $ createDirectoryIfMissing False newrelicBuildDir
-  withCurrentDirectory newrelicBuildDir $ trace "use temp dir" $ do
+  _ <- createDirectoryIfMissing False newrelicBuildDir
+  withCurrentDirectory newrelicBuildDir $ do
     let filename = "v" <> newrelicVersion <> ".tar.gz"
-    _ <- trace "download c-sdk" $
+    _ <-
       readProcess
         "curl"
         ["-OL", "https://github.com/newrelic/c-sdk/archive/" <> filename]
         ""
-    _ <- trace "unpack c-sdk" $
+    _ <-
       readProcess
         "tar"
         ["xzf", filename]
         ""
     _ <-
       withCurrentDirectory newrelicSdkDir
-        $ trace "use c-sdk dir" $ do
-          _ <- trace "fix version in c-sdk" $ readProcess
+        $ do
+          _ <- readProcess
             "sed"
             [ "-Ei"
             , "s/AGENT_VERSION.*/AGENT_VERSION := " <> newrelicVersion <> ".0/"
             , "vendor/newrelic/make/version.mk"
             ]
             ""
-          trace "build c-sdk" $ readProcess
+          readProcess
             "make"
             ["static", "CFLAGS='-Wno-missing-field-initializers'"]
             ""
-    pure $ trace "successfully finished" emptyHookedBuildInfo
+    pure emptyHookedBuildInfo
 
 -- All this does is programmatically edit "package.yaml" (or, well, the .cabal file)
 -- to add `extra-lib-dirs` and `include-dirs` pointing to the newrelic C-SDK paths
